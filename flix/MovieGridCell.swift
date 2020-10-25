8fc44e6983fbd6481081a24b85a775bbd52b1c9e
XINYAN CHEN <22_cchen@berkeley.edu>
2020-10-25 10:36:50 -0700

    twice

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Cindy on 10/25/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
